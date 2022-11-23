f1e2b568d952c7432f77932545f858b70d2b1b4e
Paul Walsh <paul@walsh.co.il>
2022-11-23 13:18:24 +0200

    Functionality to save contact form submissions.

--- config.js
+++ config.js
@@ -2,3 +2,7 @@ export const API_BASE = "https://api.squiggle.com.au";
 export const API_TEAMS_ENDPOINT = `${API_BASE}/?q=teams`;
 export const API_GAMES_ENDPOINT = `${API_BASE}/?q=games`;
 export const IMAGE_BASE = "https://squiggle.com.au";
+export const SUPABASE_URL = process.env.SUPABASE_URL;
+export const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
+export const CONTACT_FORM_ENDPOINT = "/api/contact/";
+export const CONTACT_FORM_TABLE_NAME = "afl_contact_submissions";
