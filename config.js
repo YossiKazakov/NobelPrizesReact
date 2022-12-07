bfb92e17e5fe81e09b3069f41dcc5e2a6d034ff0
YossiKazakov <96634612+YossiKazakov@users.noreply.github.com>
2022-12-07 16:28:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const SUPABASE_URL = process.env.SUPABASE_URL;
 export const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
-export const CONTACT_FORM_ENDPOINT = "/api/contact";
+export const CONTACT_FORM_ENDPOINT = "/api/contact/";
 export const CONTACT_FORM_TABLE_NAME = "Nobel_Prize_Submissions";
 export const API_PREFIX = "https://api.nobelprize.org/2.1/laureates?limit=900"
